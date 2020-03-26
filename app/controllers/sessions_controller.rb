36b61e3241d9a50d3cbbe93822f6cb3bbf8dfbbd
Elana Ryznar <eryzna@gmail.com>
2020-03-26 22:30:31 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if !params[:name] || params[:name].empty?
       redirect_to(controller: 'sessions', action: 'new')
     else
+      session[:name]=params[:name]
       redirect_to '/'
     end
   end
